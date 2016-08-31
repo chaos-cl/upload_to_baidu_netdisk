298176c31534de59ce0b2e8ee76d07b4f2579ab2
zhangpeng <zhangpeng01@51talk.com>
2016-08-31 17:32:14 +0800

    fix shell script error

--- upload.sh
+++ upload.sh
@@ -5,7 +5,7 @@ fi
 for file in `ls split`;do
 	echo "uploading $file..."
 	./bpcs_uploader/bpcs_uploader.php upload ./split/$file $file
-	if [ $? -eq 0];then
+	if [ $? -eq 0 ];then
 		echo "upload $file ok"
 		mv split/$file finish/$file
 	fi
